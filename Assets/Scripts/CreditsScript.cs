17aaf347540e595e303962d21b961dd901ec62dc
ghaiklor <ghaiklor@gmail.com>
2016-01-23 12:50:05 +0200

    Move all in single folders

--- Assets/Scripts/CreditsScript.cs
+++ Assets/Scripts/CreditsScript.cs
@@ -2,14 +2,14 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class CreditsScripts : MonoBehaviour
+public class CreditsScript : MonoBehaviour
 {
 	public int buttonWidth = 128;
 	public int buttonHeigth = 32;
 
 	void OnGUI ()
 	{
-		Rect backButton = new Rect (5, 5, buttonWidth, buttonHeigth);
+		Rect backButton = new Rect (10, 10, buttonWidth, buttonHeigth);
 
 		if (GUI.Button (backButton, "Back")) {
 			SceneManager.LoadScene ("Menu");
