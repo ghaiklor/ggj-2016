5f031a60a4b131d7f9d8c4f8ebded1a3b049e727
ghaiklor <ghaiklor@gmail.com>
2016-01-23 13:24:07 +0200

    Add Player and Tile scripts

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	private Vector2 currentDirection = Vector2.zero;
+
+	void Move ()
+	{
+		transform.Translate (currentDirection);
+	}
+
+	void Start ()
+	{
+		InvokeRepeating ("Move", 0.5f, 0.5f);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			currentDirection = Vector2.right;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			currentDirection = -Vector2.right;
+		} else {
+			currentDirection = Vector2.zero;
+		}
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour
+{
+	public int score = 2;
+}
