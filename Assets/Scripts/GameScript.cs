b5885eb8ab5d672f81660411a937b3a122cf6a32
ghaiklor <ghaiklor@gmail.com>
2016-01-23 16:08:22 +0200

    Start again the new project

--- Assets/Scripts/GameScript.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameScript : MonoBehaviour
-{
-	public GameObject[] tiles;
-}
--- Assets/Scripts/TileScript.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TileScript : MonoBehaviour
-{
-	public int score = 2;
-}
