004d2778e804d74f3c6c13695381bd121993a963
ghaiklor <ghaiklor@gmail.com>
2016-01-23 12:32:22 +0200

    Add credits and menu scenes

---
+++ Assets/Scripts/Credits/CreditsScripts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditsScripts : MonoBehaviour
+{
+	public int buttonWidth = 128;
+	public int buttonHeigth = 32;
+
+	void OnGUI ()
+	{
+		Rect backButton = new Rect (5, 5, buttonWidth, buttonHeigth);
+
+		if (GUI.Button (backButton, "Back")) {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
+}
