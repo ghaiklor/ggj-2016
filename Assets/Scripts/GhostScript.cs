540304c2a511a629fd9464f63e5673fef6183bb5
ghaiklor <ghaiklor@gmail.com>
2016-01-30 00:56:35 +0200

    Add randomize speed to candles and ghosts

--- Assets/Scripts/GhostScript.cs
+++ Assets/Scripts/GhostScript.cs
@@ -5,6 +5,11 @@ public class GhostScript : MonoBehaviour
 {
 	public float speed = 1f;
 
+	void Start ()
+	{
+		speed = (float)Random.Range (0f, 5f);
+	}
+
 	void Update ()
 	{
 		transform.Translate (0, speed * Time.deltaTime, 0);
