4e5845147e2e512f158c131bc5c221b8cf11d24e
ashaman <ashaman1991@gmail.com>
2016-01-29 19:42:50 +0200

    Background

---
+++ Assets/Scripts/BkScrollScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BkScrollScript : MonoBehaviour
+{
+	public Vector2 speed = new Vector2 (0, 0);
+	public Vector2 direction = new Vector2 (0, 0);
+
+	void Update ()
+	{
+		transform.Translate (new Vector3 (speed.x * direction.x, speed.y * direction.y, 0));
+	}
+}
