f1675ab9174ca7e8133f13f136bbc381c9eecdef
ghaiklor <ghaiklor@gmail.com>
2016-01-29 18:47:55 +0200

    Implement basic InputController

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+	public float speed = 10f;
+
+	private Rigidbody2D rb2d;
+
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		float x = Input.GetAxis ("Horizontal");
+
+		rb2d.AddForce (new Vector2 (speed * x * Time.deltaTime, 0), ForceMode2D.Force);
+	}
+}
