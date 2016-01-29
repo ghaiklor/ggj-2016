ab3a0fc8be29528bdfe07e93fc2ec60743ec0989
ghaiklor <ghaiklor@gmail.com>
2016-01-29 22:48:19 +0200

    Add ghosts prefabs and candle prefab

---
+++ Assets/Scripts/GhostScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostScript : MonoBehaviour
+{
+	private GameObject player;
+	private Rigidbody2D rb2d;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update ()
+	{
+//		Vector3 position = Vector3.SmoothDamp (transform.position, player.transform.position, ref Vector3.zero, Time.deltaTime);
+
+//		transform.Translate (position);
+	}
+}
