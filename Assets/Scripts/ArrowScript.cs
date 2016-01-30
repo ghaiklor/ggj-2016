836595b2a515d6c4a92c61a64bdc446e108bc829
ghaiklor <ghaiklor@gmail.com>
2016-01-30 09:14:57 +0200

    Ebanyu GGJ

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour
+{
+	public KeyCode validKeyCode;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (validKeyCode)) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameScript : MonoBehaviour
+{
+	public GameObject[] arrows;
+
+	void Start ()
+	{
+		InvokeRepeating ("AddNewArrow", 0f, 2f);
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	void AddNewArrow ()
+	{
+		GameObject arrow = GameObject.FindGameObjectWithTag ("Arrow");
+
+		if (arrow == null) {
+			GameObject toInstantiate = arrows [Random.Range (0, arrows.Length)];
+			Vector3 position = new Vector3 (0f, -2f, 0f);
+			Instantiate (toInstantiate, position, Quaternion.identity);
+		}
+	}
+}
