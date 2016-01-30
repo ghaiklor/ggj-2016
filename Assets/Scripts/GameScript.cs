b7d8d8100218242b068c1243ea6ff6e68b95e6d3
ghaiklor <ghaiklor@gmail.com>
2016-01-30 11:20:10 +0200

    Ebanyu GGJ

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -4,26 +4,22 @@ using System.Collections.Generic;
 
 public class GameScript : MonoBehaviour
 {
-	public GameObject[] arrows;
+	public string TextToWin = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-	void Start ()
-	{
-		InvokeRepeating ("AddNewArrow", 0f, 2f);
-	}
+	private string TypedText = "";
 
 	void Update ()
 	{
-		
-	}
-
-	void AddNewArrow ()
-	{
-		GameObject arrow = GameObject.FindGameObjectWithTag ("Arrow");
-
-		if (arrow == null) {
-			GameObject toInstantiate = arrows [Random.Range (0, arrows.Length)];
-			Vector3 position = new Vector3 (0f, -2f, 0f);
-			Instantiate (toInstantiate, position, Quaternion.identity);
+//		TextObject.transform.Translate (-Time.deltaTime * ScrollSpeed, 0, 0);
+		if (TypedText.Length <= TextToWin.Length) {
+			if (TypedText.Length > 0) {
+				if (!TypedText [TypedText.Length - 1].Equals (TextToWin [TypedText.Length - 1])) {
+//					TypedText = TypedText.Substring (0, TypedText.Length - 1);
+					Debug.Log ("FAIL");
+				}
+			}
+		} else {
+			TypedText = TypedText.Substring (0, TypedText.Length - 1);
 		}
 	}
 }
