a74fded834c20a98532e78936715618f40811f7e
ghaiklor <ghaiklor@gmail.com>
2016-01-30 14:44:23 +0200

    Ebanyu GGJ

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject player;
+	GameObject go;
+	public void Start() {
+		go = (GameObject)Instantiate (player, new Vector3 (0, 1.5f, 0), new Quaternion(0,0,-170,0));
+		go.GetComponent<MoveScript> ().enabled = false;
+	}
+	public void OnClick(int len) {
+		go.GetComponent<MoveScript> ().enabled = true;
+		StartCoroutine (Example ());
+
+	}
+
+	IEnumerator Example() {
+		yield return new WaitForSeconds (3);
+		Application.LoadLevel (1);
+	}
+}
