6a3277fbc1c2fe25042b3bc87bb3ca5e30d82d92
ghaiklor <ghaiklor@gmail.com>
2016-01-30 04:41:32 +0200

    Clean up to previous state

--- Assets/Scripts/CandleScript.cs
+++ Assets/Scripts/CandleScript.cs
@@ -10,21 +10,12 @@ public class CandleScript : MonoBehaviour
 	public float minGravityScale = -0.5f;
 	public float maxGravityScale = -0.1f;
 
-	private Light light;
 	private Rigidbody2D rb2d;
 
 	void Start ()
 	{
-		light = GetComponentInChildren<Light> ();
 		rb2d = GetComponent<Rigidbody2D> ();
 		rb2d.gravityScale = (float)Random.Range (minGravityScale, maxGravityScale);
-	}
-
-	void Update ()
-	{
-//		stepRangeLight = light.range > maxRangeLight ? -stepRangeLight : stepRangeLight;
-//
-//		light.range += stepRangeLight;
 	}
 
 	void OnBecameInvisible ()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-	public GameObject background;
 	public GameObject player;
-	public List<GameObject> candles;
 	public List<GameObject> ghosts;
 
 	private bool gameStarted = false;
@@ -16,28 +14,12 @@ public class GameManager : MonoBehaviour
 		gameStarted = true;
 
 		SpawnPlayer ();
-		StartCoroutine (SpawnRandomCandle ());
 		StartCoroutine (SpawnRandomGhost ());
-
-		float height = background.GetComponent<SpriteRenderer> ().bounds.size.y;
-		Instantiate (background, new Vector3 (0, -(height / 4.0f)), new Quaternion ());
-		Instantiate (background, new Vector3 (0, -height - (height / 4.0f)), new Quaternion ());
 	}
 
 	public void SpawnPlayer ()
 	{
 		Instantiate (player, new Vector3 (2f, 2f, 0), Quaternion.identity);
-	}
-
-	public IEnumerator SpawnRandomCandle ()
-	{
-		while (gameStarted) {
-			GameObject toInstantiate = candles [Random.Range (0, candles.Count)];
-			Vector3 position = new Vector3 (Random.Range (-5f, 5f), -10f, 0);
-			Instantiate (toInstantiate, position, Quaternion.identity);
-
-			yield return new WaitForSeconds ((int)Random.Range (0, 20));
-		}
 	}
 
 	public IEnumerator SpawnRandomGhost ()
