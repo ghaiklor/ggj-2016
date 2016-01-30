08d2f26eace3d731e05e3306a6e9363223e92b97
ghaiklor <ghaiklor@gmail.com>
2016-01-30 02:35:08 +0200

    Disable ambient light

--- Assets/Scripts/CandleScript.cs
+++ Assets/Scripts/CandleScript.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class CandleScript : MonoBehaviour
 {
-	private float speed = 1f;
+	public float minRangeLight = 1f;
+	public float maxRangeLight = 2f;
+	public float stepRangeLight = 0.1f;
+
+	public float minGravityScale = -0.5f;
+	public float maxGravityScale = -0.1f;
+
+	private Light light;
+	private Rigidbody2D rb2d;
 
 	void Start ()
 	{
-		speed = (float)Random.Range (2f, 5f);
+		light = GetComponentInChildren<Light> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.gravityScale = (float)Random.Range (minGravityScale, maxGravityScale);
 	}
 
 	void Update ()
 	{
-		transform.Translate (0, speed * Time.deltaTime, 0);
+		stepRangeLight = light.range > maxRangeLight ? -stepRangeLight : stepRangeLight;
+
+		light.range += stepRangeLight;
 	}
 
 	void OnBecameInvisible ()
