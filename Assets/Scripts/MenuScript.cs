8e945640f059434b75b97a10ee3426008f24607f
ghaiklor <ghaiklor@gmail.com>
2016-01-31 13:38:08 +0200

    Add Menu Scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+	public Animation CutSceneAnimation;
+
+	IEnumerator Start ()
+	{
+		yield return WaitForAnimation (CutSceneAnimation);
+		SceneManager.LoadScene ("Game");
+	}
+
+	public IEnumerator WaitForAnimation (Animation animation)
+	{
+		do {
+			yield return null;
+		} while (animation.isPlaying);
+	}
+}
